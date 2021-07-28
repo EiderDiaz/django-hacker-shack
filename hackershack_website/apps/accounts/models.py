9b573eac98da4050c5a3276e239203baea4653c5
eiderdiazmoraila@hotail.com <eiderdiazmoraila@hotmail.com>
2021-07-28 18:38:57 -0500

    [update] changes in profile html template to reflect the changes in the models to fetch DB data

--- hackershack_website/apps/accounts/models.py
+++ hackershack_website/apps/accounts/models.py
@@ -3,13 +3,20 @@ from django.contrib.auth.models import User
 from django.http import request
 
 
+class UserInterest(models.Model):
+    name = models.CharField(max_length=64,unique=True)
+    normalized_name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class UserPersona(models.Model):
     name = models.CharField(max_length=64,unique=True)
     normalized_name = models.CharField(max_length=64, unique=True)
     description = models.CharField(max_length=200)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
@@ -19,12 +26,15 @@ def __str__(self):
 class UserProfile(models.Model):
     #foreign key (owner)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-
+    #settings
     is_full_name_displayed = models.BooleanField(default=True)
     #details
     bio = models.CharField(max_length=500,blank=True,null=True)
     website = models.URLField(max_length=200,blank=True,null=True)
     persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+    #many profiles asociated with many interest
+    interest = models.ManyToManyField(UserInterest, blank=True)
+
 
 
 
